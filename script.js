fca32f7e512bc5fcc39e61e9e9a2454a6906cd60
mikicola <michela.colangelo@gmail.com>
2022-03-04 16:27:24 +0100

    primo step numeri random

---
+++ script.js
@@ -0,0 +1,16 @@
+// Visualizzare in pagina 5 numeri casuali.
+// Da lì parte un timer di 30 secondi. (Cioè lasciate i numeri visibili per 30 secondi allo scadere dei quali li nascondete)
+// Dopo aver nascosto i numeri chiedete all'utente (con dei prompt) di inserirli in ordine, uno alla volta.
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+//visualizzare i numeri
+arrRandomNum = []
+
+for (let i = 0; i < 5; i++) {
+    randomNum = Math.floor(Math.random() * 10 ) + 1;
+    arrRandomNum.push(randomNum)
+}
+
+
+console.log(arrRandomNum)
+
